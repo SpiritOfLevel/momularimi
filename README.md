2b5f8d5480f4badeb7f9be069a460845f65e8304
trapbreaker <qu55baiemvcb@gmail.com>
2017-07-23 05:32:38 +0900

    Support Other Arg; Move aa to readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # momularimi
+                                 .+MMN,                      .gMMm,
+                               .jMM$(MMm.                  .dMM%?MMm.
+                              .MMMMMMMMMN,                .MMMMMMMMMNx
+                             jMM!......?MMe              .MM5......,MMR.
+                           .dMMMMM""""WMMMN,            .MMMMH""""MMMMMN.
+                           dM#    .....JgMMNMMMMMMMMMMMNMMNg+(.....   HMN.
+                 ......(J+gMMNMMMMH"""7?!                  ~?77"THMMMNNMMNg+J-.....
+         ..(gNNMMMMB"""7dMMY!                                          ?TMMK7"T"WMMMMNNgJ..
+     ..NMMB"!         .dM#'                                              .MMN.         ?""MMNa, 
+   .NM#=             .MM@                                                  TMN,             ?WMN,
+    7HMNNgJ.......  .MMY         .......                    .......         ?MN&   ......(ggNMH" 
+         ?7"T"""HMMMMM$     ..gMMMMH"""MMMNJ,          ..gMMM""""HMMNmJ,     /MMMMMH"""9""! 
+                  .MM%     TH5gNMMMMMMMMNmJ?            ?j&NMMMMMMMMNmTW5     (MMo
+                 .MMF       +MM=        (TMN,          .dM@^        ?MMm.      JMN,
+                 jM#       ,MM}           dMN          MMF           .MM[       MMR
+                .MMF       .HMN,         .dM@          dMN,         .dM#!       (MN_
+                .MM)         ?WMNa.....(NM#=            ?HMNa.....JNM#=         .MM}
+                .MM]            _7""""""!                  _"""""""!            .MM:
+                 dMN.                       JMN,.   .JMN:                       dM@
+                  WMN,                        TWM#MMM8^                       .dM#!
+                   7MNe.                                                    ..MMD
+                     ?WMNx,                                              ..MM#=
+                        ?TMMNg-,.                                  ...+MMM"=
+                             ?"TMMMNNggg&J................(+gggNMMMMB"7 
+                                       ~??"THHHHHHHHHB""""7!~~
--- momyu.c
+++ momyu.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     extern char *optarg;
     extern int optind;
 
-    while ((c = (char)getopt(argc, argv, "cdflm:M")) != -1)
+    while ((c = (char)getopt(argc, argv, "cdfhlm:M")) != -1)
     {
         switch (c)
         {
@@ -27,6 +27,9 @@ int main(int argc, char *argv[])
                 break;
             case 'f':
                 printf("森のシャワー\n");
+                break;
+            case 'h':
+                printf("こんにちは！\n");
                 break;
             case 'l':
                 printf("光るどんぐり\n");
@@ -43,45 +46,9 @@ int main(int argc, char *argv[])
     }
     argc -= optind;
     argv += optind;
+  	for(i = 0; i < argc; i++)
+   	{
+   		printf("%sもみゅ\n",argv[i]);
+    }
     return 0;
 }
-
-/*                                 .,.                        ..,.
-                                 .dMMN,                      .MMMN,
-                                .MMF?MMp                    (MMDTMNe
-                               .MMNggkMMp                  (MMNggdMMp
-                              .MMMMMHMMMM|                .MMMMHMMMMMp
-                             .MMF      dMN.              .MM@      7MN;
-                            .MM@....(...MMb              (MMi..(-...dMN,
-                            dMMMMMMMMMMMMMN<            .MMMMMMMMMMMMMMb.
-                           .MM9=        (MMb...(J+JJ-...+MN!        ?TMM[                        
-                          .MM@  ...JgNMMMMMMMMMMMMMMMMMMMMMMMNNag-..  dMN-
-                      ....JMMgMMMMMMB""7!                  ??""WMMMMMNNMMm....
-             ...&gNMMMMMMMMMMB"=                                   _?TWMMMMMMMMMMNNgJ..
-         .JgMMMMMB""?!  jMM>                                            (MMR. _?7"WMMMMMNa..
-      .gMMMB"!         (MM\                                              (MMm         _"WMMMa,
-    .dMM9^            .MM%                                                (MM[            (TMMN,
-   (MM@              .MMF                                                  ?MM;              TMMp
-   .MMNmJ..         .MM@                                                    dMN-         ...gMM#^
-     _TMMMMMNNNg+J..dM#!       ..(+ggggJ..                ..JgggggJ..       .MMN..(JggNMMMMMM"!
-          _?"""MMMMMMM%     .JMMMMMB""HMMMNa.          .(MMMMH""WMMMMNg,     ,MMMMMMMB""7!
-                  .MM@     dMMHWgNMMMMNaJ?TM$          ?MB=(gMMMMMNmyHMM#     dMN.
-                  (M#!     .!(MMM9"??7TMMMe.            .(MMM9"??7TMMNe?!     .MMb
-                 .MMF       (MM%        (MMN,          .dM#^        ?MMb       dMN_
-                 (MM:      .MM%           MMb          dM#           (MM;      .MMp
-                 dM#       ,MM{           JMM          MMF           .MMr       dM#
-                 MMF       .MMb.          dM#          MMN.          (M#!       (MN.
-                .MM\        (MMN,       .dMM'          .MMN,       .dMM^        .MM{
-                .MM)         .TMMNgJ..+NMM@!            .TMMNg-.(&MMM@!         .MM}
-                .MM]            ?TMMMMMB"                  7TMMMMMB=            .MM:
-                 MMb                         ..       ..                        (M#              
-                 dMN.                       dMNa.   .dMM>                      .MM@
-                 .MMb                        ?MMMNNMMMD                        (M#:
-                  ?MMp                         -7"""^                         (MMt         
-                   ?MMm.                                                     (MM$
-                    (MMNJ                                                  .MM#^                 
-                      ?MMNa,                                            .(MMM"
-                        ?TMMNg,.                                    ..+MMM#=
-                           ?TMMMNNa...                        ...JgMMMMB= 
-                               ?TWMMMMMMNNNggJ.......JgNgNNMMMMMMMB"^                            
-                                     _?7"""HMMMMMMMMMMMHB""""!*/ 
